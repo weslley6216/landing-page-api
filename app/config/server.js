771b2221d794bfe57e55b0b5da518ff7246ce88c
Weslley Campos <weslley.campos@rebase.com.br>
2022-02-26 18:20:43 -0300

    Initial commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '18f85620332e215745b8ea7709943ba4'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '007ec682-73d0-44b3-90cd-1652c27d0c7f'
+};
